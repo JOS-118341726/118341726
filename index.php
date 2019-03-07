b995928f23dfd11f0a8eb02bb6422cf25b0c398a
118341726 <118341726@BISB62VL.bis.ad.ucc.ie>
2019-03-07 10:15:47 +0000

    74 Index Commit

--- index.php
+++ index.php
@@ -1,17 +1,41 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<head>
+    <title>Jack O'Sullivan's Index Page</title>
+<style>
+.button {
+  background-color: #ffffcc;
+  border: none;
+  color: black;
+  padding: 20px 52px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+</style>
+<link rel="stylesheet" type="text/css" href="MyStyle.css">
+</head>
+<body>
+
+<center>
+<h2>Jack O'Sullivan's IS1113: Index Page</h2>
+<br><br>
+</center>
+
+<center>
+<a href="CV.html" class="button">My CV</a>
+<br><br>
+<br><br>
+<a href="Interests.html" class="button">My Interests</a>
+<br><br>
+<br><br>
+<a href="Consultancy.html" class="button">Consultancy</a>
+</center>
+
+</body>
 </html>
+
